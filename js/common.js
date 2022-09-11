05b5095335111bd97dc170f7f972ef1aa7be4f86
hayata19791218 <hayatakyohei@kzh.biglobe.ne.jp>
2022-09-11 15:00:51 +0900

    データのアップ

---
+++ js/common.js
@@ -0,0 +1,47 @@
+"use-strict";
+
+let typingText = document.getElementById("typingText");
+const words = ["apple", "banana", "milk"];
+const miss = document.getElementById("miss");
+let missNumber = 0;
+let countDownNumber = document.getElementById("countDownNumber");
+const reset = document.querySelector(".reset");
+let state = true;
+
+typingText.innerText = words[0];
+
+window.addEventListener("keydown", function (e) {
+  let key = e.key;
+
+  if (!state) return;
+  if (key == typingText.innerText.slice(0, 1)) {
+    key = words[0].slice(1);
+    typingText.innerText = key;
+    words[0] = key;
+  } else {
+    missNumber++;
+    miss.innerText = missNumber;
+  }
+  if (words[0].length == 0) {
+    words.shift();
+    typingText.innerText = words[0];
+  }
+  if (words.length == 0) {
+    typingText.classList.add("big");
+    typingText.innerText = "お疲れ様でした";
+    reset.classList.add("block");
+    state = false;
+  }
+});
+
+let number = 3;
+countDownNumber.innerText = number;
+setInterval("textChange()", 1200);
+function textChange() {
+  number--;
+  countDownNumber.innerText = number;
+}
+
+setTimeout(function () {
+  document.querySelector(".countDown").classList.add("none");
+}, 3600);
